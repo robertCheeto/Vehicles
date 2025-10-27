f7a30eb6c2b502934cc358e6347b64db83e8c03b
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-27 11:18:15 -0400

    * created Car Child Class that's extended from the Vehicles Class * added private variables, getters/setters to the Class

---
+++ src/main/java/com/pluralsight/models/Car.java
@@ -0,0 +1,24 @@
+package com.pluralsight.models;
+
+public class Car extends Vehicles {
+    private int numberOfDoors;
+    private String carType;
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+
+}
